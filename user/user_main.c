cea37a63f28bb73aacd8063ca48acd4ed5d9319d
Mikhail Grigorev <sleuthhound@gmail.com>
2016-08-21 00:20:26 +0500

    Added support SDK v2.0.0
    Signed-off-by: Mikhail Grigorev <sleuthhound@gmail.com>

--- user/user_main.c
+++ user/user_main.c
@@ -1,16 +1,49 @@
 #include "ets_sys.h"
 #include "osapi.h"
+#include "user_interface.h"
 #include "noolite_platform.h"
 #include "driver/uart.h"
 
 extern int ets_uart_printf(const char *fmt, ...);
 int (*console_printf)(const char *fmt, ...) = ets_uart_printf;
 
-void user_rf_pre_init(void)
+uint32 ICACHE_FLASH_ATTR user_rf_cal_sector_set(void)
+{
+    enum flash_size_map size_map = system_get_flash_size_map();
+    uint32 rf_cal_sec = 0;
+
+    switch (size_map) {
+        case FLASH_SIZE_4M_MAP_256_256:
+            rf_cal_sec = 128 - 8;
+            break;
+
+        case FLASH_SIZE_8M_MAP_512_512:
+            rf_cal_sec = 256 - 5;
+            break;
+
+        case FLASH_SIZE_16M_MAP_512_512:
+        case FLASH_SIZE_16M_MAP_1024_1024:
+            rf_cal_sec = 512 - 5;
+            break;
+
+        case FLASH_SIZE_32M_MAP_512_512:
+        case FLASH_SIZE_32M_MAP_1024_1024:
+            rf_cal_sec = 1024 - 5;
+            break;
+
+        default:
+            rf_cal_sec = 0;
+            break;
+    }
+
+    return rf_cal_sec;
+}
+
+void ICACHE_FLASH_ATTR user_rf_pre_init(void)
 {
 }
 
-void user_init(void)
+void ICACHE_FLASH_ATTR user_init(void)
 {
 	uart_init(BIT_RATE_9600, BIT_RATE_9600);
 	os_delay_us(1000);
